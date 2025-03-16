e8280626d801655d9e3395d8766d0a625b338181
Park Yeonkyu <72383349+yeonkyupark@users.noreply.github.com>
2025-03-16 22:47:51 +0900

    Update sitemap-generator.py

--- sitemap-generator.py
+++ sitemap-generator.py
@@ -3,22 +3,27 @@ from urllib.parse import urljoin
 from datetime import datetime
 
 # 사이트 URL 설정
-BASE_URL = "https://yeonkyupark.github.io/"
+BASE_URL = "https://yeonkyupark.github.io/pepc2e/"
 
 # 출력 파일
+output_dir = "docs"
 output = "sitemap.xml"
 
 def generate_sitemap():
     urls = []
-    for root, dirs, files in os.walk("docs"):
+    for root, dirs, files in os.walk(output_dir):
         for file in files:
             if file.endswith(".html"):
                 file_path = os.path.join(root, file)
-                rel_path = os.path.relpath(file_path, "docs")
+                rel_path = os.path.relpath(file_path, output_dir)
                 url = urljoin(BASE_URL, rel_path.replace("\\", "/"))
                 urls.append(url)
     
-    with open(os.path.join("docs", output), "w") as f:
+    # 디렉토리 존재 여부 확인 후 생성
+    os.makedirs(output_dir, exist_ok=True)
+    
+    # sitemap.xml 생성
+    with open(os.path.join(output_dir, output), "w") as f:
         f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
         f.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n')
         for url in urls:
